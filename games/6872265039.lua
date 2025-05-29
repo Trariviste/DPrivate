b350fd566753fe64367264e997290b06f8439090
CreamyWare <svedonikadrian@gmail.com>
2025-05-29 13:24:28 +0200

    Update 6872265039.lua

--- games/6872265039.lua
+++ games/6872265039.lua
@@ -141,7 +141,7 @@ run(function()
             local loaded = Humanoid.Animator:LoadAnimation(anim)
             loaded.Priority = Enum.AnimationPriority.Action4
             loaded:Play()
-            loaded.TimePosition = 0.2
+            loaded.TimePosition = 0.77
             loaded:AdjustSpeed(0)
 
             RunService.RenderStepped:Wait()
@@ -174,7 +174,6 @@ run(function()
         Tooltip = "Reworked Invis with working toggle and respawn support"
     })
 
-    -- Reapply on respawn if still enabled
     LocalPlayer.CharacterAdded:Connect(function()
         if invisibilityEnabled then
             task.wait(0.5)
