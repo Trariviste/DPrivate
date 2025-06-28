28186b0cc14035df9a7705a09df19b485600ffa9
CreamyWare <svedonikadrian@gmail.com>
2025-06-28 16:35:38 +0200

    .

--- games/6872265039.lua
+++ games/6872265039.lua
@@ -1,4 +1,4 @@
---vape:CreateNotification('Creamyware Discontinued', 'Creamyware will no longer be updated or maintained.', 30)
+warningNotification("CreamyWare", "Test Notification", 10)
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local cam = workspace.CurrentCamera
