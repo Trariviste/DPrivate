77763a66fe3fcf23e8b7e707226644be6a52ccc8
CreamyWare <svedonikadrian@gmail.com>
2025-07-02 15:46:11 +0200

    .

--- games/6872265039.lua
+++ games/6872265039.lua
@@ -1,4 +1,3 @@
-notif('CreamyWare', 'This is a test notification dont mind it', 5, 'warning')
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local cam = workspace.CurrentCamera
