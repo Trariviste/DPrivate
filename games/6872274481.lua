2192071321dacdf8cceba986501574243d855d41
Trariviste <gifaniga@gmail.com>
2025-07-04 13:39:22 +0200

    .

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -5685,7 +5685,7 @@ run(function()
     local TimeUp: table = {["Value"] = 0.4}
     local Down: table = {["Value"] = 0.1}
     local Range: table = {["Value"] = 15}
-    Antihit = Antihit = vape.Categories.Blatant:CreateModule({
+    Antihit = vape.Categories.Blatant:CreateModule({
         ["Name"] = "Antihit",
         ["Function"] = function(callback: boolean): void
             if callback then
@@ -5693,14 +5693,14 @@ run(function()
                     while Antihit["Enabled"] do
                         local character: any = lplr["Character"];
                         if character and character:FindFirstChild("HumanoidRootPart") then
-                            local root: Humanoid? = character.HumanoidRootPart;
+                            local root: BasePart = character.HumanoidRootPart;
                             for _, v in next, playersService:GetPlayers() do
-                                if v ~= lplr and v.Team ~= lplr.Team then
+                                if v ~= lplr then
                                     local hum: any = v["Character"]
-                                    if hum and otherChar:FindFirstChild("HumanoidRootPart") then
-                                        local dist: number? = (root["Position"] - hum["HumanoidRootPart"]["Position"]).Magnitude
+                                    if hum and hum:FindFirstChild("HumanoidRootPart") then
+                                        local dist: any = (root["Position"] - hum["HumanoidRootPart"]["Position"]).Magnitude
                                         if dist <= Range["Value"] then
-                                            local org: CFrame? = root["Position"];
+                                            local org: Vector3 = root["Position"];
                                             root.CFrame = CFrame.new(org + Vector3.new(0, -90, 0))
                                             task.wait(0.4)
                                             if Antihit["Enabled"] and lplr["Character"] and lplr.Character:FindFirstChild("HumanoidRootPart") then
@@ -5720,24 +5720,24 @@ run(function()
         ["Tooltip"] = "Prevents you from dying"
     })
     Range = Antihit:CreateSlider({
-        ["Name"] = 'Gravity',
+        ["Name"] = 'Range',
         ["Min"] = 0,
         ["Max"] = 50,
-        ["Default"] = 15
+        ["Default"] = 15,
         ["Function"] = function(val) end
     })
     TimeUp = Antihit:CreateSlider({
         ["Name"] = 'Time Up',
         ["Min"] = 0,
         ["Max"] = 1,
-        ["Default"] = 0.4
+        ["Default"] = 0.4,
         ["Function"] = function(val) end
     })
     Down = Antihit:CreateSlider({
         ["Name"] = 'Time Down',
         ["Min"] = 0,
         ["Max"] = 1,
-        ["Default"] = 0.1
+        ["Default"] = 0.1,
         ["Function"] = function(val) end
     })    
 end)
