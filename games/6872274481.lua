98fa4ac3e60b26fd546187b64be638a5667f6aed
Trariviste <gifaniga@gmail.com>
2025-07-06 13:42:32 +0200

    .

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -12955,7 +12955,7 @@ run(function()
                         if blink then
                             blink:Toggle()
                         end
-                        task.wait(0.4)
+                        task.wait(0.5)
                     end
                 end)
             else
