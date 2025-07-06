20b89d8c5cd81415d85d1e75809e4c7f5e4e9c37
Trariviste <gifaniga@gmail.com>
2025-07-06 13:29:15 +0200

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -12955,7 +12955,7 @@ run(function()
                         if blink then
                             blink:Toggle() -- Correct method call with colon
                         end
-                        task.wait(0.2)
+                        task.wait(0.8)
                     end
                 end)
             else
